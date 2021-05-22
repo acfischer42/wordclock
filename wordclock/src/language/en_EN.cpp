2e420ab6352354a55d6962068b337d9c15d82768
Achim <who@cares.com>
2021-05-22 11:31:33 +0200

    fixed time of day
    todo - noon

--- wordclock/src/language/en_EN.cpp
+++ wordclock/src/language/en_EN.cpp
@@ -6,6 +6,8 @@
 #include "../dnd.h"
 #include "../led.h"
 
+int Grid_en_EN::time_day_ix = 0;
+
 void Grid_en_EN::setTime(int hour, int minute) {
   if(hour == -1 || minute == -1) {
 	return;
@@ -39,9 +41,30 @@ Serial.println("grid_en_EN::settime");
   }
 //need to put logic to show afternoon / evening here
 //int Grid_en_EN::time_day[12][6]
+
+  if(hour == 0) {
+    time_day_ix = 0;
+  }
+  if(hour >= 0 && hour <= 11) {
+    time_day_ix = 1;
+  }
+  if(hour == 12) {
+    time_day_ix = 2;
+  }
+  if(hour >= 13 && hour <= 18) {
+    time_day_ix = 3;
+  }
+  if(hour >= 19 && hour <= 21) {
+    time_day_ix = 4;
+  }
+  if(hour >= 22) {
+    time_day_ix = 5;
+  }
+  Serial.println(time_day_ix);
+
   for(int i = 0; i < 14; i++) {
-  if(Grid_en_EN::time_day[3][i] >= 0) {
-    Led::ids[Led::getLedId(Grid_en_EN::time_day[3][i])].setRGB(Config::color_fg.r, Config::color_fg.g, Config::color_fg.b);
+  if(Grid_en_EN::time_day[time_day_ix][i] >= 0) {
+    Led::ids[Led::getLedId(Grid_en_EN::time_day[time_day_ix][i])].setRGB(Config::color_fg.r, Config::color_fg.g, Config::color_fg.b);
 
   }
   }
@@ -77,10 +100,13 @@ Serial.println("grid_en_EN::settime");
 	  Led::ids[Led::getLedId(Grid_en_EN::time_hours[hour][h])].setRGB(Config::color_fg.r, Config::color_fg.g, Config::color_fg.b);
 	}
   }
-
+/*
+Serial.println("GRID_SINGLE_MINUTES");
+Serial.println(singleMinute);
   if(GRID_SINGLE_MINUTES == 1) {
 	// single minutes
 	for(int s = (NUM_LEDS - 4); s < (NUM_LEDS - 4 + singleMinute); s++) {
+    Serial.println(s);
 	  Led::ids[s].setRGB(Config::color_fg.r, Config::color_fg.g, Config::color_fg.b);
 	}
   } else {
@@ -88,17 +114,17 @@ Serial.println("grid_en_EN::settime");
 	  Led::ids[s].setRGB(Config::color_fg.r, Config::color_fg.g, Config::color_fg.b);
 	}
   }
-
+*/
   //FastLED.setBrightness(Config::brightness * 255);
   FastLED.setBrightness(50);
-  Serial.println("Fastled.show");
+
   FastLED.show();
 }
 
 int Grid_en_EN::time_it_is[5] = {0, 1, 3, 4,-1}; // it is
 
 int Grid_en_EN::time_minutes[12][15] = {
-  {107, 108, 109,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1, -1, -1}, // uhr
+  { -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1, -1, -1}, // uhr
   { 16,  17,  18,  19,  32,  33,  34,  35,  -1,  -1,  -1,  -1, -1, -1, -1}, // five past
   {  6,   7,   8,  32,  33,  34,  35,  -1,  -1,  -1,  -1,  -1, -1, -1, -1}, // ten past
   { 22,  25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35, -1, -1, -1}, // a quarter past
@@ -127,17 +153,11 @@ int Grid_en_EN::time_hours[12][6] = {
   {  52,	53,	 54,	55,	56,	57}  // eleven
 };
 
-int Grid_en_EN::time_day[12][14] = {
-  {  87, 88,   99, 100,	101, 102,	103,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // at night
+int Grid_en_EN::time_day[6][14] = {
+  {  96, 97,	 98,	99,	100, 101,	102, 103,  -1,  -1,  -1,  -1,  -1,  -1}, // midnight
   {  90, 91, 	 93,	94,	 95, 104,	105, 106,	107, 108,	109, 110,  -1,  -1}, // in the morning
-  {  90, 91, 	 93,	94,	 95, 121,	122, 123,	124, 125, 126, 127,  -1,  -1}, // in the evening
+  { 117,118,	119, 120,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // noon
   {  90, 91, 	 93,	94,	 95, 112,	113, 114,	115, 116,	117, 118,	119, 120}, // in the afternoon
-  {  64,  65,  66,  67,  -1,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // four
-  {  68,	69,	 70,	71,  -1,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // five
-  {  58,	59,	 60,  -1,  -1,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // six
-  {  72,	73,	 74,	75,	 76,  65,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // seven
-  {  42,	43,	 44,	45,	 46,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // eight
-  {  76,	77,	 78,	79,  -1,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // nine
-  {  61,	62,	 63,  -1,  -1,  -1,  -1, -1, -1, -1, -1, -1,  -1,  -1}, // ten
-  {  52,	53,	 54,	55,	 56,	57,  -1, -1, -1, -1, -1, -1,  -1,  -1}  // eleven
+  {  90, 91, 	 93,	94,	 95, 121,	122, 123,	124, 125, 126, 127,  -1,  -1}, // in the evening
+  {  87, 88,   99, 100,	101, 102,	103,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // at night
 };
--- wordclock/src/language/en_EN.h
+++ wordclock/src/language/en_EN.h
@@ -6,8 +6,8 @@ class Grid_en_EN {
 	static int time_it_is[5];
 	static int time_minutes[12][15];
 	static int time_hours[12][6];
-	static int time_day[12][14];
-	static int hourix;
+	static int time_day[6][14];
+	static int time_day_ix;
 	static int minuteix;
 	static void setTime(int hour, int minute);
 };
